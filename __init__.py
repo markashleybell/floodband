20d7f073ea31bcb452910aaef37227497f5753cd
Mark Bell <me@markb.co.uk>
2018-05-02 08:07:03 +0100

    Get things working under Python 3

--- __init__.py
+++ __init__.py
@@ -1,4 +1,5 @@
 import re
+import os
 import json
 import soundcloud
 from flask import Flask, Response, render_template, request, send_from_directory, redirect, url_for, jsonify, flash
@@ -92,7 +93,7 @@ def statuses():
             return jsonify({ 'error': e._msg, 'error_code': e.error_code, 'status_code': e.status_code, 'headers': e.headers, 'resource_url': e.resource_url })
 
     for s in statuses:
-        s['text'] = re.sub(ur"(@([^\s\:\,\.\u2014]+))", r'<a href="https://twitter.com/\2">\1</a>', s['text'], 0, re.IGNORECASE | re.MULTILINE)
+        s['text'] = re.sub("(@([^\s\:\,\.\u2014]+))", r'<a href="https://twitter.com/\2">\1</a>', s['text'], 0, re.IGNORECASE | re.MULTILINE)
         for u in s['entities']['urls']:
             s['text'] = re.sub(u['url'], r'<a href="' + u['expanded_url'] + '">' + u['display_url'] + '</a>', s['text'], 0, re.IGNORECASE | re.MULTILINE)
         if 'media' in s['entities']:
