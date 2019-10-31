4246d03e1531eaef7f2d74134b8ffe3f674f006d
Mark Bell <me@markb.co.uk>
2019-10-31 11:03:17 +0000

    Fixes for GH security alerts
    None of the vulnerable code was ever actually deployed, this just stops me getting emails about it...

--- gulpfile.js
+++ gulpfile.js
@@ -15,7 +15,6 @@ const config = {
             outputStyle: 'expanded'
         },
         autoprefixerOptions: {
-            browsers: ['> 1%', 'last 2 versions', 'Firefox ESR', 'Opera 12.1'],
             cascade: false
         }
     },
